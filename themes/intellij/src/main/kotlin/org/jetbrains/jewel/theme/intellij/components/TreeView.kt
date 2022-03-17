93f2fa7e2f2a06c6abc8dd10cdcb7f8f0c7a64d5
Lamberto basti <basti.lamberto@gmail.com>
2022-03-17 11:23:30 +0100

    Focus with keyboard done!

--- themes/intellij/src/main/kotlin/org/jetbrains/jewel/theme/intellij/components/TreeView.kt
+++ themes/intellij/src/main/kotlin/org/jetbrains/jewel/theme/intellij/components/TreeView.kt
@@ -19,6 +19,8 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.mouseClickable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -33,20 +35,29 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEvent
+import androidx.compose.ui.input.key.KeyEventType
+import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onKeyEvent
-import androidx.compose.ui.input.pointer.PointerButtons
+import androidx.compose.ui.input.key.type
 import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.PointerKeyboardModifiers
+import androidx.compose.ui.input.pointer.isCtrlPressed
+import androidx.compose.ui.input.pointer.isMetaPressed
 import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.times
+import org.jetbrains.jewel.theme.intellij.appendIf
 import org.jetbrains.jewel.theme.intellij.styles.LocalTreeViewStyle
 import org.jetbrains.jewel.theme.intellij.styles.TreeViewState
 import org.jetbrains.jewel.theme.intellij.styles.TreeViewStyle
 import org.jetbrains.jewel.theme.intellij.styles.updateTreeViewAppearanceTransition
+import org.jetbrains.jewel.theme.intellij.visibleItemsRange
+import org.jetbrains.skiko.hostOs
 import java.io.File
 import java.nio.file.Path
+import kotlin.math.max
+import kotlin.math.min
 
 @Immutable
 data class Tree<T>(val heads: List<Element<T>>) {
@@ -161,6 +172,7 @@ fun <T> TreeView(
     onKeyPressed: (KeyEvent, Int, Tree.ElementWithDepth<T>) -> Boolean = { _, _, _ -> false },
     style: TreeViewStyle = LocalTreeViewStyle.current,
     state: LazyListState = rememberLazyListState(),
+    focusedTreeElement: Tree.Element<T>? = null,
     tree: Tree<T>,
     onTreeNodeToggle: (Tree.Element.Node<T>) -> Unit,
     onTreeElementClick: MouseClickScope.(Tree.Element<T>) -> Unit,
@@ -171,32 +183,31 @@ fun <T> TreeView(
 
     val appearance = style.appearance(isFocused)
     val appearanceTransitionState = updateTreeViewAppearanceTransition(appearance)
+    val columnFocusRequester: FocusRequester = remember { FocusRequester() }
 
     LazyColumn(
-        modifier = modifier,
+        modifier = modifier
+            .focusable()
+            .focusRequester(columnFocusRequester)
+            .onFocusChanged { isFocused = TreeViewState.fromBoolean(it.isFocused || it.hasFocus) },
         state = state
     ) {
-        itemsIndexed(tree.flattenedTree) { index, treeElementWithDepth ->
+        itemsIndexed(tree.flattenedTree, key = { _, item -> item }) { index, treeElementWithDepth ->
             val (treeElement, depth) = treeElementWithDepth
             var isElementSelected by remember { mutableStateOf(false) }
             val focusRequester: FocusRequester = remember { FocusRequester() }
 
             Row(
                 modifier = Modifier
-                    .appendIf(treeElement.isSelected) { background(appearanceTransitionState.selectedBackground) }
-                    .focusRequester(focusRequester)
-                    .onPointerEvent(PointerEventType.Press) { focusRequester.requestFocus() }
-                    .onFocusChanged { focusState ->
-                        isElementSelected = focusState.isFocused
-                        isFocused = TreeViewState.fromBoolean(isElementSelected)
-                    }
                     .focusable()
+                    .focusRequester(focusRequester)
+                    .appendIf(treeElement.isSelected) { background(appearanceTransitionState.selectedBackground) }
+                    .onFocusChanged { isElementSelected = it.isFocused }
+                    .onPointerEvent(PointerEventType.Press) { focusRequester.requestFocus() }
                     .onKeyEvent { onKeyPressed(it, index, treeElementWithDepth) }
-                    .mouseClickable {
-                        focusRequester.requestFocus()
-                        onTreeElementClick(treeElement)
-                    }
+                    .mouseClickable { onTreeElementClick(treeElement) }
                     .appendIf(isElementSelected) { border(2.dp, Color.Red) }
+
 //                    .combinedClickable(
 //                        onClick = { EmptyClickContext.onTreeElementClick(treeElement) },
 //                        onDoubleClick = { onTreeElementDoubleClick(treeElement) }
@@ -220,13 +231,145 @@ fun <T> TreeView(
                     }
                 }
             }
+
+            if (focusedTreeElement == treeElement) {
+                LaunchedEffect(focusedTreeElement) {
+                    focusRequester.requestFocus()
+                }
+            }
+        }
+    }
+
+    // show focused element if present so that it can actually be focused
+    if (focusedTreeElement != null) {
+        LaunchedEffect(focusedTreeElement) {
+            // get index of focusedTreeElement that will be focused
+            val indexOfFocusedItem = tree.flattenedTree.indexOfFirst { it.treeElement == focusedTreeElement }
+
+            // if focusedTreeElement has been found in the flattened tree
+            if (indexOfFocusedItem >= 0) {
+
+                // drop preloaded first and last item indexes that are not really visible
+                // the second dropped is to match IJ tree view behaviour
+                val visibleRange = state.visibleItemsRange.drop(2).dropLast(4)
+
+                // if focusedTreeElement is not visible then show it
+                if (indexOfFocusedItem !in visibleRange && visibleRange.isNotEmpty()) {
+                    when {
+                        // if focusedTreeElement is above the shown ones
+                        indexOfFocusedItem < visibleRange.first() -> state.scrollToItem(max(0, indexOfFocusedItem - 2))
+                        // if focusedTreeElement is below
+                        indexOfFocusedItem > visibleRange.last() -> {
+                            // indexOfFirstVisibleWhenFocusedTreeElementIsLast
+                            val indexOfFirstVisibleElement = indexOfFocusedItem - visibleRange.size
+                            state.scrollToItem(min(tree.flattenedTree.lastIndex, indexOfFirstVisibleElement - 1))
+                        }
+                    }
+                }
+            }
         }
     }
 }
 
-internal fun Modifier.appendIf(condition: Boolean, transformer: Modifier.() -> Modifier): Modifier =
-    if (!condition) this else transformer()
+// Temporary name, it is a TreeView but with default behaviours for key presses
+// and opening of the nodes
+@Composable
+fun <T> NavigableTreeView(
+    modifier: Modifier = Modifier,
+    onKeyPressed: (KeyEvent, Int, Tree.ElementWithDepth<T>) -> Boolean = { _, _, _ -> false },
+    style: TreeViewStyle = LocalTreeViewStyle.current,
+    state: LazyListState = rememberLazyListState(),
+    treeMutableState: MutableState<Tree<T>>,
+    onTreeElementDoubleClick: (Tree.Element<T>) -> Unit,
+    content: @Composable RowScope.(Tree.Element<T>) -> Unit
+) {
+    var focusedTreeElement: Tree.Element<T>? by remember { mutableStateOf(null) }
 
-private val EmptyClickContext = MouseClickScope(
-    PointerButtons(0), PointerKeyboardModifiers(0)
-)
+    var tree by treeMutableState
+
+    val onTreeNodeToggle: (Tree.Element.Node<T>) -> Unit = { tree = tree.replaceElement(it, it.copy(isOpen = !it.isOpen)) }
+
+    TreeView(
+        modifier = modifier,
+        onKeyPressed = { keyEvent, index, elementWithDepth ->
+            val (element, depth) = elementWithDepth
+
+            fun moveFocus(newIndex: Int): Boolean {
+                tree = tree.selectOnly(tree.flattenedTree[newIndex].treeElement)
+
+                // retrieve the item only after invoking selectOnly(), the item will be different since it's copied
+                focusedTreeElement = tree.flattenedTree[newIndex].treeElement
+                return true
+            }
+
+            when {
+                keyEvent.type != KeyEventType.KeyDown -> false
+                keyEvent.key == Key.Enter -> {
+                    onTreeElementDoubleClick(element)
+                    true
+                }
+                index > 0 && keyEvent.key == Key.DirectionUp -> moveFocus(index - 1)
+                index < tree.flattenedTree.lastIndex && keyEvent.key == Key.DirectionDown -> moveFocus(index + 1)
+                keyEvent.key == Key.DirectionRight -> when {
+                    element is Tree.Element.Node<T> && !element.isOpen -> {
+                        onTreeNodeToggle(element)
+                        focusedTreeElement = tree.flattenedTree[index].treeElement
+                        true
+                    }
+                    index < tree.flattenedTree.lastIndex -> moveFocus(index + 1)
+                    else -> false
+                }
+                keyEvent.key == Key.DirectionLeft -> when {
+                    element is Tree.Element.Node<T> && element.isOpen -> {
+                        onTreeNodeToggle(element)
+                        focusedTreeElement = tree.flattenedTree[index].treeElement
+                        true
+                    }
+                    index > 0 -> when (element) {
+                        !in tree.heads -> {
+                            var currentIndex = index
+                            val parentDepth = depth - 1
+                            while (currentIndex > 0) {
+                                currentIndex--
+                                if (tree.flattenedTree[currentIndex].depth == parentDepth) {
+                                    break
+                                }
+                            }
+                            check(currentIndex >= 0) { "Cannot find parent of $element" }
+                            moveFocus(currentIndex)
+                        }
+                        else -> moveFocus(0)
+                    }
+                    else -> false
+                }
+                keyEvent.key == Key.Home -> moveFocus(0)
+                keyEvent.key == Key.MoveEnd -> moveFocus(tree.flattenedTree.lastIndex)
+                keyEvent.key == Key.PageDown -> moveFocus(index + state.layoutInfo.visibleItemsInfo.size)
+                keyEvent.key == Key.PageUp -> moveFocus(index - state.layoutInfo.visibleItemsInfo.size)
+                else -> onKeyPressed(keyEvent, index, elementWithDepth)
+            }
+        },
+        style = style,
+        state = state,
+        focusedTreeElement = focusedTreeElement,
+        tree = tree,
+        onTreeNodeToggle = onTreeNodeToggle,
+        onTreeElementClick = {
+            val isMultiSelectionKeyPressed = when {
+                hostOs.isWindows || hostOs.isLinux -> keyboardModifiers.isCtrlPressed
+                hostOs.isMacOS -> keyboardModifiers.isMetaPressed
+                else -> false
+            }
+            tree = when {
+                isMultiSelectionKeyPressed -> {
+                    val elements =
+                        tree.flattenedTree.filter { it.treeElement.isSelected }.map { it.treeElement }.toSet()
+                    tree.selectElements(if (it.isSelected) elements - it else elements + it)
+                }
+                else -> tree.selectOnly(it)
+            }
+        },
+        onTreeElementDoubleClick = onTreeElementDoubleClick,
+        content = content
+    )
+}
