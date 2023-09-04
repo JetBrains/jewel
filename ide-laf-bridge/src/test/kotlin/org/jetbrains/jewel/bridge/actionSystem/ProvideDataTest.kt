671077ae5931bf927a3018d617bf92c9e7437ba6
Nikolay Rykunov <nikolay.rykunov@jetbrains.com>
2023-09-04 20:16:21 +0200

    Fix detektTest issue with Unit type

--- ide-laf-bridge/src/test/kotlin/org/jetbrains/jewel/bridge/actionSystem/ProvideDataTest.kt
+++ ide-laf-bridge/src/test/kotlin/org/jetbrains/jewel/bridge/actionSystem/ProvideDataTest.kt
@@ -22,90 +22,94 @@ class ProvideDataTest {
     val rule = createComposeRule()
 
     @Test
-    fun `one component`() = runBlocking {
-        val rootDataProviderModifier = RootDataProviderModifier()
-        var focusManager: FocusManager? = null
-        rule.setContent {
-            focusManager = LocalFocusManager.current
-            Box(
-                modifier = Modifier
-                    .then(rootDataProviderModifier)
-                    .testTag("provider")
-                    .provideData {
-                        when (it) {
-                            "data" -> "ok"
-                            else -> null
+    fun `one component`() {
+        runBlocking {
+            val rootDataProviderModifier = RootDataProviderModifier()
+            var focusManager: FocusManager? = null
+            rule.setContent {
+                focusManager = LocalFocusManager.current
+                Box(
+                    modifier = Modifier
+                        .then(rootDataProviderModifier)
+                        .testTag("provider")
+                        .provideData {
+                            when (it) {
+                                "data" -> "ok"
+                                else -> null
+                            }
                         }
-                    }
-                    .focusable(),
-            )
+                        .focusable(),
+                )
+            }
+            rule.awaitIdle()
+            focusManager!!.moveFocus(FocusDirection.Next)
+            rule.awaitIdle()
+
+            rule.onNodeWithTag("provider").assertIsFocused()
+
+            assertEquals("ok", rootDataProviderModifier.dataProvider("data"))
+            assertEquals(null, rootDataProviderModifier.dataProvider("another_data"))
         }
-        rule.awaitIdle()
-        focusManager!!.moveFocus(FocusDirection.Next)
-        rule.awaitIdle()
-
-        rule.onNodeWithTag("provider").assertIsFocused()
-
-        assertEquals("ok", rootDataProviderModifier.dataProvider("data"))
-        assertEquals(null, rootDataProviderModifier.dataProvider("another_data"))
     }
 
     @Test
-    fun `component hierarchy`() = runBlocking {
-        val rootDataProviderModifier = RootDataProviderModifier()
-        var focusManager: FocusManager? = null
-        rule.setContent {
-            focusManager = LocalFocusManager.current
-            Box(
-                modifier = Modifier
-                    .then(rootDataProviderModifier)
-                    .testTag("root_provider")
-                    .provideData {
-                        when (it) {
-                            "isRoot" -> "yes"
-                            else -> null
+    fun `component hierarchy`() {
+        runBlocking {
+            val rootDataProviderModifier = RootDataProviderModifier()
+            var focusManager: FocusManager? = null
+            rule.setContent {
+                focusManager = LocalFocusManager.current
+                Box(
+                    modifier = Modifier
+                        .then(rootDataProviderModifier)
+                        .testTag("root_provider")
+                        .provideData {
+                            when (it) {
+                                "isRoot" -> "yes"
+                                else -> null
+                            }
                         }
+                        .focusable(),
+                ) {
+                    Box(modifier = Modifier.testTag("non_data_provider").focusable()) {
+                        Box(
+                            modifier = Modifier
+                                .testTag("data_provider_item")
+                                .provideData {
+                                    when (it) {
+                                        "data" -> "ok"
+                                        else -> null
+                                    }
+                                }.focusable(),
+                        )
                     }
-                    .focusable(),
-            ) {
-                Box(modifier = Modifier.testTag("non_data_provider").focusable()) {
-                    Box(
-                        modifier = Modifier
-                            .testTag("data_provider_item")
-                            .provideData {
-                                when (it) {
-                                    "data" -> "ok"
-                                    else -> null
-                                }
-                            }.focusable(),
-                    )
                 }
             }
+
+            rule.awaitIdle()
+            focusManager!!.moveFocus(FocusDirection.Next)
+            rule.awaitIdle()
+
+            rule.onNodeWithTag("root_provider").assertIsFocused()
+            assertEquals("yes", rootDataProviderModifier.dataProvider("isRoot"))
+            assertEquals(null, rootDataProviderModifier.dataProvider("data"))
+
+            focusManager!!.moveFocus(FocusDirection.Next)
+            rule.awaitIdle()
+
+            rule.onNodeWithTag("non_data_provider").assertIsFocused()
+            // non_data_provider still should provide isRoot == true because it should be taken from root
+            // but shouldn't provide "data" yet
+            assertEquals("yes", rootDataProviderModifier.dataProvider("isRoot"))
+            assertEquals(null, rootDataProviderModifier.dataProvider("data"))
+
+            focusManager!!.moveFocus(FocusDirection.Next)
+            rule.awaitIdle()
+
+            rule.onNodeWithTag("data_provider_item").assertIsFocused()
+
+            assertEquals("yes", rootDataProviderModifier.dataProvider("isRoot"))
+            assertEquals("ok", rootDataProviderModifier.dataProvider("data"))
         }
-
-        rule.awaitIdle()
-        focusManager!!.moveFocus(FocusDirection.Next)
-        rule.awaitIdle()
-
-        rule.onNodeWithTag("root_provider").assertIsFocused()
-        assertEquals("yes", rootDataProviderModifier.dataProvider("isRoot"))
-        assertEquals(null, rootDataProviderModifier.dataProvider("data"))
-
-        focusManager!!.moveFocus(FocusDirection.Next)
-        rule.awaitIdle()
-
-        rule.onNodeWithTag("non_data_provider").assertIsFocused()
-        // non_data_provider still should provide isRoot == true because it should be taken from root
-        // but shouldn't provide "data" yet
-        assertEquals("yes", rootDataProviderModifier.dataProvider("isRoot"))
-        assertEquals(null, rootDataProviderModifier.dataProvider("data"))
-
-        focusManager!!.moveFocus(FocusDirection.Next)
-        rule.awaitIdle()
-
-        rule.onNodeWithTag("data_provider_item").assertIsFocused()
-
-        assertEquals("yes", rootDataProviderModifier.dataProvider("isRoot"))
-        assertEquals("ok", rootDataProviderModifier.dataProvider("data"))
     }
 }
