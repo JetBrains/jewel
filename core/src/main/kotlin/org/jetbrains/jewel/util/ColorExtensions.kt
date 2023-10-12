72a4645c74912644ace62d5c4486df9ea5d096c4
higan <higan@live.cn>
2023-10-12 22:16:24 +0800

    Make detekt happy

--- core/src/main/kotlin/org/jetbrains/jewel/util/ColorExtensions.kt
+++ core/src/main/kotlin/org/jetbrains/jewel/util/ColorExtensions.kt
@@ -22,13 +22,10 @@ fun Color.toHexString(): String {
     }
 }
 
-fun Color.isDark(): Boolean {
-    return (luminance + 0.05) / 0.05 < 4.5
-}
+fun Color.isDark(): Boolean = (luminance + 0.05) / 0.05 < 4.5
 
 val Color.luminance: Double
     get() = linearRGBComponentValue(red) * 0.2126 + linearRGBComponentValue(green) * 0.7152 + linearRGBComponentValue(blue) * 0.0722
 
-private fun linearRGBComponentValue(colorValue: Float): Double {
-    return if (colorValue <= 0.03928) colorValue / 12.92 else ((colorValue + 0.055) / 1.055).pow(2.4)
-}
+private fun linearRGBComponentValue(colorValue: Float): Double =
+    if (colorValue <= 0.03928) colorValue / 12.92 else ((colorValue + 0.055) / 1.055).pow(2.4)
