9b7d13d5c0cb11039333b88ebfd4e8a40c473ea0
Daniele Conti <fourlastor@users.noreply.github.com>
2024-01-27 11:12:46 +0100

    Cross build via construo

--- samples/standalone/build.gradle.kts
+++ samples/standalone/build.gradle.kts
@@ -1,10 +1,13 @@
 @file:Suppress("UnstableApiUsage")
 
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import io.github.fourlastor.construo.Target
 
 plugins {
     jewel
     alias(libs.plugins.composeDesktop)
+    alias(libs.plugins.construo)
+    alias(libs.plugins.shadow)
 }
 
 dependencies {
@@ -49,6 +52,45 @@ tasks {
                 vendor = JvmVendorSpec.JETBRAINS
             }
             setExecutable(javaLauncher.map { it.executablePath.asFile.absolutePath }.get())
+        }
+    }
+}
+
+construo {
+    // name of the executable
+    name.set("jewel")
+    // human readable name, used for example in the `.app` name for MacOS
+    humanName.set("Jewel Standalone")
+    // version, required if not specified in project
+    version.set("1.0.0")
+    // main class, it wouldn't be required if compose used the application plugin
+    mainClass.set("org.jetbrains.jewel.samples.standalone.MainKt")
+    roast {
+        // required to get around compose using context class loader
+        useMainAsContextClassLoader.set(true)
+    }
+    jlink {
+        // required as it's used via string by compose, so it's not picked up by jlink automatically
+        modules.addAll("jdk.zipfs")
+    }
+    targets {
+        // Linux X64
+        create<Target.Linux>("linuxX64") {
+            architecture.set(Target.Architecture.X86_64)
+            jdkUrl.set("https://cache-redirector.jetbrains.com/intellij-jbr/jbr-17.0.10-linux-x86-b1087.17.tar.gz")
+        }
+        // macOS M1
+        create<Target.MacOs>("macOsM1") {
+            architecture.set(Target.Architecture.AARCH64)
+            identifier.set("org.jetbrains.jewel.sample.standalone")
+            macIcon.set(file("icons/jewel.icns"))
+            jdkUrl.set("https://cache-redirector.jetbrains.com/intellij-jbr/jbr-17.0.10-osx-aarch64-b1087.17.tar.gz")
+        }
+        // Windows x64
+        create<Target.Windows>("winx64") {
+            useGpuHint.set(false)
+            architecture.set(Target.Architecture.X86_64)
+            jdkUrl.set("https://cache-redirector.jetbrains.com/intellij-jbr/jbr-17.0.10-windows-x64-b1087.17.tar.gz")
         }
     }
 }
