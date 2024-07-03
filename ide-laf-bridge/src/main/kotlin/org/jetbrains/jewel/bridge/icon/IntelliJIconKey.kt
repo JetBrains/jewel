c5c9d82bc09bf73f2f07c6eca2f2a9bd1f293a54
Sebastiano Poggi <sebp@google.com>
2024-07-03 21:46:33 +0200

    Remove unnecessary suppression

--- ide-laf-bridge/src/main/kotlin/org/jetbrains/jewel/bridge/icon/IntelliJIconKey.kt
+++ ide-laf-bridge/src/main/kotlin/org/jetbrains/jewel/bridge/icon/IntelliJIconKey.kt
@@ -6,7 +6,6 @@ import org.jetbrains.jewel.ui.icon.IntelliJIconKey
 
 @Suppress("UnstableApiUsage") // We need to use internal APIs
 public fun IntelliJIconKey.Companion.fromPlatformIcon(icon: javax.swing.Icon): IconKey {
-    @Suppress("UnstableApiUsage") // Not much we can do
     check(icon is CachedImageIcon) {
         "Only resource-backed CachedImageIcons are supported (e.g., coming from AllIcons)"
     }
