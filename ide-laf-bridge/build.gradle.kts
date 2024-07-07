31255b18461e95712de3e6b1286a7a47e3f28b80
Sebastiano Poggi <sebp@google.com>
2024-07-07 16:22:43 +0100

    Revert IJ Gradle plugin to v1.17.3
    v2.0 causes a ClassDefNotFoundException for the Bridge dependency (but
    not its transitive deps) when using the 2.0 plugin with pre-242 IJP
    versions.
    
    Until that's fixed on the IJ Gradle plugin 2.0 side, we can't use it for
    pre-242 IJP versions. This change does NOT affect the main branch.

--- ide-laf-bridge/build.gradle.kts
+++ ide-laf-bridge/build.gradle.kts
@@ -4,19 +4,6 @@ plugins {
     `jewel-check-public-api`
     `ide-version-checker`
     alias(libs.plugins.composeDesktop)
-    alias(libs.plugins.ideaPluginModule)
-}
-
-// Because we need to define IJP dependencies, the dependencyResolutionManagement
-// from settings.gradle.kts is overridden and we have to redeclare everything here.
-repositories {
-    google()
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    mavenCentral()
-
-    intellijPlatform {
-        defaultRepositories()
-    }
 }
 
 dependencies {
@@ -24,9 +11,7 @@ dependencies {
         exclude(group = "org.jetbrains.kotlinx")
     }
 
-    intellijPlatform {
-        intellijIdeaCommunity(libs.versions.idea)
-    }
+    compileOnly(libs.bundles.idea)
 
     testImplementation(compose.desktop.uiTestJUnit4)
     testImplementation(compose.desktop.currentOs) {
