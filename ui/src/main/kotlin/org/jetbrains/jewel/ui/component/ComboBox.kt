30f0e187b478abc5aea8cc377c50c962ed74d44f
Ivan Morgillo <imorgillo@gmail.com>
2024-09-17 17:18:14 +0200

    reformat
    Signed-off-by: Ivan Morgillo <imorgillo@gmail.com>

--- ui/src/main/kotlin/org/jetbrains/jewel/ui/component/ComboBox.kt
+++ ui/src/main/kotlin/org/jetbrains/jewel/ui/component/ComboBox.kt
@@ -99,54 +99,49 @@ public fun ComboBox(
     var componentWidth by remember { mutableIntStateOf(-1) }
     Box(
         modifier =
-        modifier
-            .clickable(
-                onClick = {
-                    // TODO: Trick to skip click event when close menu by click dropdown
-                    if (!skipNextClick) {
-                        popupExpanded = !popupExpanded
-                    }
-                    skipNextClick = false
-                },
-                enabled = enabled,
-                role = Role.Button,
-                interactionSource = interactionSource,
-                indication = null,
-            )
-            .background(colors.backgroundFor(comboBoxState).value, shape)
-            .thenIf(outline == Outline.None) {
-                focusOutline(
+            modifier
+                .clickable(
+                    onClick = {
+                        // TODO: Trick to skip click event when close menu by click dropdown
+                        if (!skipNextClick) {
+                            popupExpanded = !popupExpanded
+                        }
+                        skipNextClick = false
+                    },
+                    enabled = enabled,
+                    role = Role.Button,
+                    interactionSource = interactionSource,
+                    indication = null,
+                )
+                .background(colors.backgroundFor(comboBoxState).value, shape)
+                .thenIf(outline == Outline.None) {
+                    focusOutline(state = comboBoxState, outlineShape = shape, alignment = Stroke.Alignment.Center)
+                }
+                .thenIf(hasNoOutline) {
+                    border(
+                        alignment = Stroke.Alignment.Inside,
+                        width = style.metrics.borderWidth,
+                        color = borderColor,
+                        shape = shape,
+                    )
+                }
+                .outline(
                     state = comboBoxState,
+                    outline = outline,
                     outlineShape = shape,
                     alignment = Stroke.Alignment.Center,
                 )
-            }
-            .thenIf(hasNoOutline) {
-                border(
-                    alignment = Stroke.Alignment.Inside,
-                    width = style.metrics.borderWidth,
-                    color = borderColor,
-                    shape = shape,
-                )
-            }
-            .outline(
-                state = comboBoxState,
-                outline = outline,
-                outlineShape = shape,
-                alignment = Stroke.Alignment.Inside,
-            )
-            .width(IntrinsicSize.Max)
-            .defaultMinSize(minSize.width, minSize.height)
-            .onSizeChanged { componentWidth = it.width },
+                .width(IntrinsicSize.Max)
+                .defaultMinSize(minSize.width, minSize.height)
+                .onSizeChanged { componentWidth = it.width },
         contentAlignment = Alignment.CenterStart,
     ) {
         CompositionLocalProvider(LocalContentColor provides colors.contentFor(comboBoxState).value) {
             Box(
                 modifier =
-                    Modifier.fillMaxWidth()
-                        .padding(end = arrowMinSize.width).onFocusChanged {
-                            comboBoxState = comboBoxState.copy(focused = it.isFocused)
-                        },
+                    Modifier.fillMaxWidth().padding(end = arrowMinSize.width).onFocusChanged {
+                        comboBoxState = comboBoxState.copy(focused = it.isFocused)
+                    },
                 contentAlignment = Alignment.CenterStart,
                 content = {
                     BasicTextField(
@@ -171,8 +166,9 @@ public fun ComboBox(
                     thickness = metrics.borderWidth,
                     color = colors.border,
                     modifier =
-                        Modifier.align(Alignment.CenterStart)
-                            .thenIf(comboBoxState.isFocused) { padding(vertical = 2.dp) },
+                        Modifier.align(Alignment.CenterStart).thenIf(comboBoxState.isFocused) {
+                            padding(vertical = 2.dp)
+                        },
                 )
                 Icon(key = style.icons.chevronDown, contentDescription = null, tint = colors.iconTint)
             }
