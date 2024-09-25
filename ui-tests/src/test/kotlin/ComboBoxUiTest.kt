31612e76d5eea6e7d9824f1b72474cc6cc3d7cbb
Ivan Morgillo <imorgillo@gmail.com>
2024-09-25 15:29:22 +0200

    start adding tests to ComboBox
    Signed-off-by: Ivan Morgillo <imorgillo@gmail.com>

--- ui-tests/src/test/kotlin/ComboBoxUiTest.kt
+++ ui-tests/src/test/kotlin/ComboBoxUiTest.kt
@@ -13,12 +13,15 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.SemanticsNodeInteraction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsFocused
 import androidx.compose.ui.test.assertIsNotFocused
+import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performKeyInput
 import androidx.compose.ui.unit.dp
 import org.jetbrains.jewel.intui.standalone.theme.IntUiTheme
@@ -28,6 +31,7 @@ import org.jetbrains.jewel.ui.component.separator
 import org.junit.Rule
 import org.junit.Test
 
+@OptIn(ExperimentalTestApi::class)
 class ComboBoxUiTest {
     @get:Rule val composeRule = createComposeRule()
 
@@ -41,7 +45,7 @@ class ComboBoxUiTest {
 
                 val itemsComboBox = remember {
                     listOf(
-                        "Cat",
+                        "First element",
                         "Elephant",
                         "Sun",
                         "Book",
@@ -79,7 +83,6 @@ class ComboBoxUiTest {
                 )
             }
         }
-
         focusRequester.requestFocus()
         composeRule.onNodeWithTag("Pre-Box").assertIsDisplayed().assertIsFocused().performKeyInput {
             keyDown(Key.Tab)
@@ -87,11 +90,115 @@ class ComboBoxUiTest {
         }
 
         composeRule.onNodeWithTag("ComboBox").assertIsDisplayed().assertIsNotFocused()
-        composeRule.onNodeWithText("Cat").assertIsDisplayed().assertIsFocused().performKeyInput {
+        composeRule.onNodeWithText("First element").assertIsDisplayed().assertIsFocused().performKeyInput {
             keyDown(Key.Tab)
             keyUp(Key.Tab)
         }
 
         composeRule.onNodeWithTag("Pre-Box").assertIsFocused()
+    }
+
+    @Test
+    fun `when not-editable both Box and TextField are focused`() {
+        val comboBox = focusedNotEditableComboBox()
+        composeRule.onNodeWithText("First element").assertIsDisplayed().assertIsFocused()
+    }
+
+    @Test
+    fun `when not-editable click opens popup`() {
+        val comboBox = focusedNotEditableComboBox()
+        comboBox.performClick()
+        composeRule.onNodeWithText("Elephant").isDisplayed()
+    }
+
+    @Test
+    fun `when not-editable double click opens and closes popup`() {
+        val comboBox = focusedNotEditableComboBox()
+
+        comboBox.performClick()
+        composeRule.onNodeWithText("Elephant").isDisplayed()
+
+        comboBox.performClick()
+        composeRule.onNodeWithText("Elephant").assertDoesNotExist()
+    }
+
+    @Test
+    fun `when not-editable pressing spacebar opens popup`() {
+        val comboBox = focusedNotEditableComboBox()
+
+        composeRule.onNodeWithText("Elephant").assertDoesNotExist()
+
+        comboBox.performKeyInput {
+            keyDown(Key.Spacebar)
+            keyUp(Key.Spacebar)
+        }
+        composeRule.onNodeWithText("Elephant").assertIsDisplayed()
+    }
+
+    @Test
+    fun `when not-editable pressing enter opens popup`() {
+        val comboBox = focusedNotEditableComboBox()
+
+        composeRule.onNodeWithText("Elephant").assertDoesNotExist()
+
+        comboBox.performKeyInput {
+            keyDown(Key.Enter)
+            keyUp(Key.Enter)
+        }
+        composeRule.onNodeWithText("Elephant").assertIsDisplayed()
+    }
+
+    private fun focusedNotEditableComboBox(): SemanticsNodeInteraction {
+        val focusRequester = FocusRequester()
+        injectComboBox(focusRequester, false)
+        focusRequester.requestFocus()
+        val comboBox = composeRule.onNodeWithTag("ComboBox")
+        comboBox.assertIsDisplayed().assertIsFocused()
+        return comboBox
+    }
+
+    private fun injectComboBox(focusRequester: FocusRequester, isEditable: Boolean) {
+        composeRule.setContent {
+            IntUiTheme {
+                val itemsComboBox = remember {
+                    listOf(
+                        "First element",
+                        "Elephant",
+                        "Sun",
+                        "Book",
+                        "Laughter",
+                        "Whisper",
+                        "Ocean",
+                        "Serendipity lorem ipsum",
+                        "Umbrella",
+                        "Joy",
+                    )
+                }
+                var selectedComboBox: String? by remember { mutableStateOf(itemsComboBox.first()) }
+                val inputTextFieldState = rememberTextFieldState(itemsComboBox.first())
+                ComboBox(
+                    isEditable = isEditable,
+                    modifier = Modifier.width(140.dp).testTag("ComboBox").focusRequester(focusRequester),
+                    inputTextFieldState = inputTextFieldState,
+                    menuContent = {
+                        itemsComboBox.forEach {
+                            if (it == "---") {
+                                separator()
+                            } else {
+                                selectableItem(
+                                    selected = selectedComboBox == it,
+                                    onClick = {
+                                        selectedComboBox = it
+                                        inputTextFieldState.edit { replace(0, length, it) }
+                                    },
+                                ) {
+                                    Text(it)
+                                }
+                            }
+                        }
+                    },
+                )
+            }
+        }
     }
 }
