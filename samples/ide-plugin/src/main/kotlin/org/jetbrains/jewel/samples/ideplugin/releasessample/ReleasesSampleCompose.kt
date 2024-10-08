d03b1970c5b492943545abf74cecc8e14fac93ff
Ivan Morgillo <imorgillo@gmail.com>
2024-10-08 16:56:11 +0200

    update IDE plugin example
    Signed-off-by: Ivan Morgillo <imorgillo@gmail.com>

--- samples/ide-plugin/src/main/kotlin/org/jetbrains/jewel/samples/ideplugin/releasessample/ReleasesSampleCompose.kt
+++ samples/ide-plugin/src/main/kotlin/org/jetbrains/jewel/samples/ideplugin/releasessample/ReleasesSampleCompose.kt
@@ -298,7 +298,7 @@ private fun OverflowMenu(currentContentSource: ContentSource<*>, onContentSource
 
     var menuVisible by remember { mutableStateOf(false) }
 
-    // Emulates Swing actions that pop up menus â€” they stay pressed while the menu is open
+    // Emulates Swing actions that pop up menus — they stay pressed while the menu is open
     IconButton(
         modifier =
             Modifier.fillMaxHeight().thenIf(menuVisible) {
