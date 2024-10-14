83fc4787b72b919de5bf6b639c6c119dad1393ae
Ivan Morgillo <imorgillo@gmail.com>
2024-10-14 16:57:55 +0200

    iterate on ListComboBox height
    Signed-off-by: Ivan Morgillo <imorgillo@gmail.com>

--- ui/src/main/kotlin/org/jetbrains/jewel/ui/component/ListComboBox.kt
+++ ui/src/main/kotlin/org/jetbrains/jewel/ui/component/ListComboBox.kt
@@ -3,7 +3,6 @@ package org.jetbrains.jewel.ui.component
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.text.input.rememberTextFieldState
@@ -18,7 +17,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import org.jetbrains.jewel.foundation.lazy.SelectionMode
 import org.jetbrains.jewel.foundation.lazy.items
@@ -93,7 +91,7 @@ public fun ListComboBox(
             VerticallyScrollableContainer(
                 scrollState = scrollState.lazyListState,
                 modifier =
-                    Modifier.height(138.dp).onHover {
+                    Modifier.heightIn(max = popupMaxHeight).onHover {
                         isListHovered = it
                         onListHoverChange(it)
                     },
