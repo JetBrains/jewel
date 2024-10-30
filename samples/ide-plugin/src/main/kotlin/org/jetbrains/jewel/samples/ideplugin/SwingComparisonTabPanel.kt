4370b2412c2ff0c623cb48697b27eabfd9d2066e
morgillo <morgillo@google.com>
2024-10-30 16:04:53 +0000

    add Swing Split Button samples to SwingComparisonTabPanel
    Signed-off-by: morgillo <morgillo@google.com>

--- samples/ide-plugin/src/main/kotlin/org/jetbrains/jewel/samples/ideplugin/SwingComparisonTabPanel.kt
+++ samples/ide-plugin/src/main/kotlin/org/jetbrains/jewel/samples/ideplugin/SwingComparisonTabPanel.kt
@@ -1,24 +1,17 @@
 package org.jetbrains.jewel.samples.ideplugin
 
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
@@ -27,9 +20,10 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import com.intellij.ide.ui.laf.darcula.ui.DarculaButtonUI
-import com.intellij.openapi.ui.ComboBox
+import com.intellij.openapi.ui.messages.MessageDialog
 import com.intellij.ui.JBColor
 import com.intellij.ui.components.JBLabel
+import com.intellij.ui.components.JBOptionButton
 import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.dsl.builder.AlignY
 import com.intellij.ui.dsl.builder.COLUMNS_SHORT
@@ -42,24 +36,19 @@ import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.components.BorderLayoutPanel
 import icons.IdeSampleIconKeys
 import icons.JewelIcons
-import javax.swing.BoxLayout
-import javax.swing.DefaultComboBoxModel
-import javax.swing.JLabel
-import javax.swing.JPanel
+import java.awt.event.ActionEvent
+import javax.swing.AbstractAction
+import javax.swing.Action
 import org.jetbrains.jewel.bridge.JewelComposePanel
 import org.jetbrains.jewel.bridge.medium
 import org.jetbrains.jewel.foundation.theme.JewelTheme
 import org.jetbrains.jewel.ui.component.DefaultButton
 import org.jetbrains.jewel.ui.component.Icon
-import org.jetbrains.jewel.ui.component.ListComboBox
-import org.jetbrains.jewel.ui.component.ListItemState
 import org.jetbrains.jewel.ui.component.OutlinedButton
-import org.jetbrains.jewel.ui.component.SimpleListItem
 import org.jetbrains.jewel.ui.component.Text
 import org.jetbrains.jewel.ui.component.TextArea
 import org.jetbrains.jewel.ui.component.TextField
 import org.jetbrains.jewel.ui.component.Typography
-import org.jetbrains.jewel.ui.theme.simpleListItemStyle
 import org.jetbrains.jewel.ui.theme.textAreaStyle
 
 internal class SwingComparisonTabPanel : BorderLayoutPanel() {
@@ -74,9 +63,6 @@ internal class SwingComparisonTabPanel : BorderLayoutPanel() {
                 textFieldsRow()
                 separator()
                 textAreasRow()
-                separator()
-                comboBoxesRow()
-                separator()
             }
             .apply {
                 border = JBUI.Borders.empty(0, 10)
@@ -98,16 +84,33 @@ internal class SwingComparisonTabPanel : BorderLayoutPanel() {
     }
 
     private fun Panel.buttonsRow() {
-        row("Buttons:") {
-                button("Swing Button") {}.align(AlignY.CENTER)
-                compose { OutlinedButton({}) { Text("Compose Button") } }
-
-                button("Default Swing Button") {}
+        row("Buttons - Swing:") {
+                button("Button") {}.align(AlignY.CENTER)
+                button("Default Button") {}
                     .align(AlignY.CENTER)
                     .applyToComponent { putClientProperty(DarculaButtonUI.DEFAULT_STYLE_KEY, true) }
-                compose { DefaultButton({}) { Text("Default Compose Button") } }
+
+                val options = arrayOf(action("Action 1"), action("Action 2"), action("Action 3"))
+                cell(JBOptionButton(action("Split").apply { isEnabled = true }, options))
+                cell(JBOptionButton(action("Default Split").apply { isEnabled = true }, options)).applyToComponent {
+                    putClientProperty(DarculaButtonUI.DEFAULT_STYLE_KEY, true)
+                }
             }
             .layout(RowLayout.PARENT_GRID)
+
+        row("Buttons - Compose:") {
+                compose { OutlinedButton({}) { Text("Button") } }
+                compose { DefaultButton({}) { Text("Default Button") } }
+            }
+            .layout(RowLayout.PARENT_GRID)
+    }
+
+    private fun action(text: String): Action {
+        return object : AbstractAction(text) {
+            override fun actionPerformed(e: ActionEvent?) {
+                MessageDialog(null, "Invoked $text", text, emptyArray<String>(), -1, null, false).show()
+            }
+        }
     }
 
     private fun Panel.labelsRows() {
@@ -135,7 +138,7 @@ internal class SwingComparisonTabPanel : BorderLayoutPanel() {
                         modifier =
                             Modifier.width(
                                 with(LocalDensity.current) {
-                                    // Guesstimate how wide this should be ? we can't tell it to be
+                                    // Guesstimate how wide this should be — we can't tell it to be
                                     // "fill", as it crashes natively
                                     JewelTheme.defaultTextStyle.fontSize.toDp() * 60
                                 }
@@ -157,7 +160,7 @@ internal class SwingComparisonTabPanel : BorderLayoutPanel() {
                         modifier =
                             Modifier.width(
                                 with(LocalDensity.current) {
-                                    // Guesstimate how wide this should be ? we can't tell it to be
+                                    // Guesstimate how wide this should be — we can't tell it to be
                                     // "fill", as it crashes natively
                                     style.fontSize.toDp() * 10
                                 }
@@ -192,170 +195,6 @@ internal class SwingComparisonTabPanel : BorderLayoutPanel() {
                 compose {
                     val state = rememberTextFieldState("")
                     TextField(state)
-                }
-            }
-            .layout(RowLayout.PARENT_GRID)
-    }
-
-    private fun Panel.comboBoxesRow() {
-        row("ComboBoxes:") {
-                // Swing ComboBoxes
-                val zoomLevels = arrayOf("100%", "125%", "150%", "175%", "200%", "300%")
-
-                JPanel()
-                    .apply {
-                        layout = BoxLayout(this, BoxLayout.Y_AXIS)
-                        add(JLabel("Not editable").apply { alignmentX = LEFT_ALIGNMENT })
-                        add(
-                            ComboBox(DefaultComboBoxModel(zoomLevels)).apply {
-                                isEditable = false
-                                alignmentX = LEFT_ALIGNMENT
-                            }
-                        )
-                    }
-                    .run { cell(this).align(AlignY.TOP) }
-                JPanel()
-                    .apply {
-                        layout = BoxLayout(this, BoxLayout.Y_AXIS)
-                        add(JLabel("Not editable + disabled").apply { alignmentX = LEFT_ALIGNMENT })
-                        add(
-                            ComboBox(DefaultComboBoxModel(zoomLevels)).apply {
-                                isEditable = false
-                                isEnabled = false
-                                alignmentX = LEFT_ALIGNMENT
-                            }
-                        )
-                    }
-                    .run { cell(this).align(AlignY.TOP) }
-
-                val itemsComboBox = arrayOf("Cat", "Elephant", "Sun", "Book", "Laughter")
-                JPanel()
-                    .apply {
-                        layout = BoxLayout(this, BoxLayout.Y_AXIS)
-                        add(JLabel("Editable").apply { alignmentX = LEFT_ALIGNMENT })
-                        add(
-                            ComboBox(DefaultComboBoxModel(itemsComboBox)).apply {
-                                isEditable = true
-                                alignmentX = LEFT_ALIGNMENT
-                            }
-                        )
-                    }
-                    .run { cell(this).align(AlignY.TOP) }
-
-                JPanel()
-                    .apply {
-                        layout = BoxLayout(this, BoxLayout.Y_AXIS)
-                        add(JLabel("Editable + Disabled").apply { alignmentX = LEFT_ALIGNMENT })
-                        add(
-                            ComboBox(DefaultComboBoxModel(itemsComboBox)).apply {
-                                isEditable = true
-                                isEnabled = false
-                                alignmentX = LEFT_ALIGNMENT
-                            }
-                        )
-                    }
-                    .run { cell(this).align(AlignY.TOP) }
-
-                compose(modifier = Modifier.height(200.dp).padding(horizontal = 8.dp, vertical = 0.dp)) {
-                    val comboBoxItems = remember {
-                        listOf(
-                            "Cat",
-                            "Elephant",
-                            "Sun",
-                            "Book",
-                            "Laughter",
-                            "Whisper",
-                            "Ocean",
-                            "Serendipity lorem ipsum",
-                            "Umbrella",
-                            "Joy",
-                        )
-                    }
-                    var selectedComboBox1: String? by remember { mutableStateOf(comboBoxItems.first()) }
-                    var selectedComboBox2: String? by remember { mutableStateOf(comboBoxItems.first()) }
-                    var selectedComboBox3: String? by remember { mutableStateOf(comboBoxItems.first()) }
-
-                    Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                        Column {
-                            Text("Not editable")
-                            Text(text = "Selected item: $selectedComboBox2")
-
-                            ListComboBox(
-                                items = comboBoxItems,
-                                modifier = Modifier.width(200.dp),
-                                isEditable = false,
-                                onSelectedItemChange = { selectedComboBox2 = it },
-                                listItemContent = { item, isSelected, isFocused, isItemHovered, isListHovered ->
-                                    SimpleListItem(
-                                        text = item,
-                                        style = JewelTheme.simpleListItemStyle,
-                                        state = ListItemState(isSelected, isListHovered, isItemHovered),
-                                        contentDescription = item,
-                                    )
-                                },
-                            )
-                        }
-
-                        Column {
-                            Text("Not editable + disabled")
-                            Text(text = "Selected item: $selectedComboBox2")
-
-                            ListComboBox(
-                                items = comboBoxItems,
-                                modifier = Modifier.width(200.dp),
-                                isEditable = false,
-                                isEnabled = false,
-                                onSelectedItemChange = { selectedComboBox2 = it },
-                                listItemContent = { item, isSelected, isFocused, isItemHovered, isListHovered ->
-                                    SimpleListItem(
-                                        text = item,
-                                        style = JewelTheme.simpleListItemStyle,
-                                        state = ListItemState(isSelected, isListHovered, isItemHovered),
-                                        contentDescription = item,
-                                    )
-                                },
-                            )
-                        }
-
-                        Column {
-                            Text("Editable")
-                            Text(text = "Selected item: $selectedComboBox1")
-                            ListComboBox(
-                                items = comboBoxItems,
-                                modifier = Modifier.width(200.dp),
-                                maxPopupHeight = 150.dp,
-                                onSelectedItemChange = { selectedComboBox1 = it },
-                                listItemContent = { item, isSelected, isFocused, isItemHovered, isListHovered ->
-                                    SimpleListItem(
-                                        text = item,
-                                        style = JewelTheme.simpleListItemStyle,
-                                        state = ListItemState(isSelected, isListHovered, isItemHovered),
-                                        contentDescription = item,
-                                    )
-                                },
-                            )
-                        }
-
-                        Column {
-                            Text("Editable + disabled")
-                            Text(text = "Selected item: $selectedComboBox3")
-                            ListComboBox(
-                                items = comboBoxItems,
-                                modifier = Modifier.width(200.dp),
-                                isEditable = true,
-                                isEnabled = false,
-                                onSelectedItemChange = { selectedComboBox3 = it },
-                                listItemContent = { item, isSelected, isFocused, isItemHovered, isListHovered ->
-                                    SimpleListItem(
-                                        text = item,
-                                        style = JewelTheme.simpleListItemStyle,
-                                        state = ListItemState(isSelected, isListHovered, isItemHovered),
-                                        contentDescription = item,
-                                    )
-                                },
-                            )
-                        }
-                    }
                 }
             }
             .layout(RowLayout.PARENT_GRID)
@@ -397,6 +236,6 @@ internal class SwingComparisonTabPanel : BorderLayoutPanel() {
     private fun PaddingValues.horizontal(layoutDirection: LayoutDirection): Dp =
         calculateStartPadding(layoutDirection) + calculateEndPadding(layoutDirection)
 
-    private fun Row.compose(modifier: Modifier = Modifier.padding(8.dp), content: @Composable () -> Unit) =
-        cell(JewelComposePanel { Box(modifier) { content() } }.apply { isOpaque = false })
+    private fun Row.compose(content: @Composable () -> Unit) =
+        cell(JewelComposePanel { Box(Modifier.padding(8.dp)) { content() } }.apply { isOpaque = false })
 }
