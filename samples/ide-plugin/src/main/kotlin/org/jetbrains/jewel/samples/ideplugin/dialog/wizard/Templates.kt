3f80a6cdb4d0bc3ca5387a785fed44a388d2e38a
Chris Sinco <csinco@google.com>
2024-11-11 21:49:33 -0800

    Add template descriptions

--- samples/ide-plugin/src/main/kotlin/org/jetbrains/jewel/samples/ideplugin/dialog/wizard/Templates.kt
+++ samples/ide-plugin/src/main/kotlin/org/jetbrains/jewel/samples/ideplugin/dialog/wizard/Templates.kt
@@ -24,15 +24,47 @@ enum class SampleCategories {
 
 data class Template(val name: String, val description: String? = null, val imageKey: PathIconKey)
 
-val noActivityTemplate = Template(name = "Empty", imageKey = TemplateIllustrations.NoActivity)
+val noActivityTemplate = Template(
+    name = "Empty",
+    description = "An empty project",
+    imageKey = TemplateIllustrations.NoActivity
+)
+
 val PhoneTemplates =
     listOf(
-        noActivityTemplate,
-        Template(name = "App", imageKey = TemplateIllustrations.EmptyActivity),
-        Template(name = "Adaptive", imageKey = TemplateIllustrations.ResponsiveLayout),
-        Template(name = "Gemini Starter App", imageKey = TemplateIllustrations.GeminiStarter),
-        Template(name = "Fullscreen App", imageKey = TemplateIllustrations.FullscreenActivity),
-        Template(name = "Google Maps", imageKey = TemplateIllustrations.GoogleMapsActivity),
-        Template(name = "Game", imageKey = TemplateIllustrations.GameActivity),
-        Template(name = "C++", imageKey = TemplateIllustrations.Cpp),
+        Template(
+            name = "App",
+            description = "Basic app structure",
+            imageKey = TemplateIllustrations.EmptyActivity
+        ),
+        Template(
+            name = "Adaptive",
+            description = "Adaptive layout for responsive design",
+            imageKey = TemplateIllustrations.ResponsiveLayout
+        ),
+        Template(
+            name = "Gemini Starter App",
+            description = "A starter app for the Gemini framework",
+            imageKey = TemplateIllustrations.GeminiStarter
+        ),
+        Template(
+            name = "Fullscreen App",
+            description = "App with a fullscreen UI",
+            imageKey = TemplateIllustrations.FullscreenActivity
+        ),
+        Template(
+            name = "Google Maps",
+            description = "Project with Google Maps integration",
+            imageKey = TemplateIllustrations.GoogleMapsActivity
+        ),
+        Template(
+            name = "Game",
+            description = "Basic game project",
+            imageKey = TemplateIllustrations.GameActivity
+        ),
+        Template(
+            name = "C++",
+            description = "Project using C++",
+            imageKey = TemplateIllustrations.Cpp
+        ),
     )
