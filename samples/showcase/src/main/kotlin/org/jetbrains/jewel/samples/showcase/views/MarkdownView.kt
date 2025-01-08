36d8c698836d913ff90558d9cd242bdcaf4a9a1a
Ivan Morgillo <imorgillo@gmail.com>
2025-01-08 11:49:52 +0100

    extract MarkdownView
    Signed-off-by: Ivan Morgillo <imorgillo@gmail.com>

---
+++ samples/showcase/src/main/kotlin/org/jetbrains/jewel/samples/showcase/views/MarkdownView.kt
@@ -0,0 +1,42 @@
+package org.jetbrains.jewel.samples.showcase.views
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.jetbrains.jewel.foundation.modifier.trackActivation
+import org.jetbrains.jewel.foundation.theme.JewelTheme
+import org.jetbrains.jewel.markdown.rendering.MarkdownBlockRenderer
+import org.jetbrains.jewel.markdown.rendering.MarkdownStyling
+import org.jetbrains.jewel.samples.showcase.markdown.JewelReadme
+import org.jetbrains.jewel.samples.showcase.markdown.MarkdownEditor
+import org.jetbrains.jewel.samples.showcase.markdown.MarkdownPreview
+import org.jetbrains.jewel.ui.Orientation
+import org.jetbrains.jewel.ui.component.Divider
+
+@Composable
+public fun MarkdownDemo(
+    darkStyling: MarkdownStyling,
+    darkRenderer: MarkdownBlockRenderer,
+    lightStyling: MarkdownStyling,
+    lightRenderer: MarkdownBlockRenderer,
+) {
+    Row(Modifier.trackActivation().fillMaxSize().background(JewelTheme.globalColors.panelBackground)) {
+        val editorState = rememberTextFieldState(JewelReadme)
+        MarkdownEditor(state = editorState, modifier = Modifier.fillMaxHeight().weight(1f))
+
+        Divider(Orientation.Vertical, Modifier.fillMaxHeight())
+
+        MarkdownPreview(
+            modifier = Modifier.fillMaxHeight().weight(1f),
+            rawMarkdown = editorState.text,
+            darkStyling = darkStyling,
+            darkRenderer = darkRenderer,
+            lightStyling = lightStyling,
+            lightRenderer = lightRenderer,
+        )
+    }
+}
--- samples/standalone/src/main/kotlin/org/jetbrains/jewel/samples/standalone/viewmodel/MainViewModel.kt
+++ samples/standalone/src/main/kotlin/org/jetbrains/jewel/samples/standalone/viewmodel/MainViewModel.kt
@@ -5,10 +5,20 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
-import org.jetbrains.jewel.samples.standalone.IntUiThemes
+import org.jetbrains.jewel.intui.markdown.standalone.dark
+import org.jetbrains.jewel.intui.markdown.standalone.light
+import org.jetbrains.jewel.intui.markdown.standalone.styling.dark
+import org.jetbrains.jewel.intui.markdown.standalone.styling.extensions.github.alerts.dark
+import org.jetbrains.jewel.intui.markdown.standalone.styling.extensions.github.alerts.light
+import org.jetbrains.jewel.intui.markdown.standalone.styling.light
+import org.jetbrains.jewel.markdown.extensions.github.alerts.AlertStyling
+import org.jetbrains.jewel.markdown.extensions.github.alerts.GitHubAlertRendererExtension
+import org.jetbrains.jewel.markdown.rendering.MarkdownBlockRenderer
+import org.jetbrains.jewel.markdown.rendering.MarkdownStyling
 import org.jetbrains.jewel.samples.showcase.components.StandaloneSampleIcons
+import org.jetbrains.jewel.samples.showcase.views.MarkdownDemo
+import org.jetbrains.jewel.samples.standalone.IntUiThemes
 import org.jetbrains.jewel.samples.standalone.view.ComponentsView
-import org.jetbrains.jewel.samples.standalone.view.MarkdownDemo
 import org.jetbrains.jewel.samples.standalone.view.WelcomeView
 
 object MainViewModel {
@@ -52,6 +62,23 @@ private val mainMenuItems =
             iconKey = StandaloneSampleIcons.markdown,
             keyboardShortcut =
                 KeyBinding(macOs = setOf("⌥", "M"), windows = setOf("Alt", "M"), linux = setOf("Alt", "M")),
-            content = { MarkdownDemo() },
+            content = {
+                MarkdownDemo(
+                    darkStyling = MarkdownStyling.dark(),
+                    darkRenderer =
+                        MarkdownBlockRenderer.dark(
+                            styling = MarkdownStyling.dark(),
+                            rendererExtensions =
+                                listOf(GitHubAlertRendererExtension(AlertStyling.dark(), MarkdownStyling.dark())),
+                        ),
+                    lightStyling = MarkdownStyling.light(),
+                    lightRenderer =
+                        MarkdownBlockRenderer.light(
+                            styling = MarkdownStyling.light(),
+                            rendererExtensions =
+                                listOf(GitHubAlertRendererExtension(AlertStyling.light(), MarkdownStyling.light())),
+                        ),
+                )
+            },
         ),
     )
