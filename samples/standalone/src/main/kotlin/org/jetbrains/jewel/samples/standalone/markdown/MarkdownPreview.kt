3eb2b4a3f20c2bebe946e6de8437d8ef8d3310ef
Ivan Morgillo <imorgillo@gmail.com>
2025-01-20 10:38:04 +0100

    clean up MarkdownPreview
    reference https://github.com/JetBrains/jewel/pull/742#discussion_r1920272965
    
    Signed-off-by: Ivan Morgillo <imorgillo@gmail.com>

--- samples/standalone/src/main/kotlin/org/jetbrains/jewel/samples/standalone/markdown/MarkdownPreview.kt
+++ samples/standalone/src/main/kotlin/org/jetbrains/jewel/samples/standalone/markdown/MarkdownPreview.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,11 +14,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.jetbrains.jewel.foundation.ExperimentalJewelApi
-import org.jetbrains.jewel.foundation.code.highlighting.CodeHighlighter
-import org.jetbrains.jewel.foundation.code.highlighting.LocalCodeHighlighter
 import org.jetbrains.jewel.foundation.code.highlighting.NoOpCodeHighlighter
 import org.jetbrains.jewel.foundation.theme.JewelTheme
+import org.jetbrains.jewel.intui.markdown.standalone.ProvideMarkdownStyling
 import org.jetbrains.jewel.intui.markdown.standalone.dark
 import org.jetbrains.jewel.intui.markdown.standalone.light
 import org.jetbrains.jewel.intui.markdown.standalone.styling.dark
@@ -29,9 +26,6 @@ import org.jetbrains.jewel.intui.markdown.standalone.styling.light
 import org.jetbrains.jewel.markdown.LazyMarkdown
 import org.jetbrains.jewel.markdown.MarkdownBlock
 import org.jetbrains.jewel.markdown.extension.autolink.AutolinkProcessorExtension
-import org.jetbrains.jewel.markdown.extensions.LocalMarkdownBlockRenderer
-import org.jetbrains.jewel.markdown.extensions.LocalMarkdownProcessor
-import org.jetbrains.jewel.markdown.extensions.LocalMarkdownStyling
 import org.jetbrains.jewel.markdown.extensions.github.alerts.AlertStyling
 import org.jetbrains.jewel.markdown.extensions.github.alerts.GitHubAlertProcessorExtension
 import org.jetbrains.jewel.markdown.extensions.github.alerts.GitHubAlertRendererExtension
@@ -98,24 +92,5 @@ fun MarkdownPreview(modifier: Modifier = Modifier, rawMarkdown: CharSequence) {
                 onUrlClick = { url -> Desktop.getDesktop().browse(URI.create(url)) },
             )
         }
-    }
-}
-
-@ExperimentalJewelApi
-@Composable
-fun ProvideMarkdownStyling(
-    markdownStyling: MarkdownStyling,
-    markdownBlockRenderer: MarkdownBlockRenderer,
-    codeHighlighter: CodeHighlighter,
-    markdownProcessor: MarkdownProcessor = remember { MarkdownProcessor() },
-    content: @Composable () -> Unit,
-) {
-    CompositionLocalProvider(
-        LocalMarkdownStyling provides markdownStyling,
-        LocalMarkdownProcessor provides markdownProcessor,
-        LocalMarkdownBlockRenderer provides markdownBlockRenderer,
-        LocalCodeHighlighter provides codeHighlighter,
-    ) {
-        content()
     }
 }
